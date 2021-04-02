110af93fa54f65c4565e364c27c4cb4634d98f99
carolvmoran <carolvandrade@gmail.com>
2021-04-02 17:38:35 -0400

    Criando Formulários

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    $("#minhaTabela").hide();
+    $("#newS").hide();
     $("#imgMenu").click(function(){
       if($(this).attr("src") == "../img/menu/menu-open.svg") 
         $(this).attr("src","../img/menu/menu.svg");
@@ -6,3 +8,26 @@ $(document).ready(function(){
         $(this).attr("src","../img/menu/menu-open.svg");
     });
   });
+function consultaCep() {
+  let cep = document.getElementById("InputCep").value;
+  let url = "https://viacep.com.br/ws/" + cep + "/json/";
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function(response){
+      $("#logradouro").html(response.logradouro);
+      $("#bairro").html(response.bairro);
+      $("#localidade").html(`${response.localidade}/${response.uf}`);
+      $("#cep").html(response.cep);
+      $("#meuForm").hide();
+      $("#minhaTabela").show();
+      $("#newS").show();
+    },
+  });
+};
+function newSearch(){
+  $("#meuForm").show();
+  $("#meuForm input").val("");
+  $("#minhaTabela").hide();
+  $("#newS").hide();
+}
